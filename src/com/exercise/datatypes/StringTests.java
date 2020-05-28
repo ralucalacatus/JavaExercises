61f817f5b89a820b9ffd7f59e3dbff45ebfa8c0f
Raluca Lacatus <raluca.lacatusM@yahoo.com>
2020-05-28 21:48:28 +0300

    package

---
+++ src/com/exercise/datatypes/StringTests.java
@@ -0,0 +1,15 @@
+package com.exercise.datatypes;
+
+public class StringTests {
+
+    //get index position for character
+    //get char at position
+    //get string size
+    //convert to upper chars
+    //print reverse string
+    //find string inside string
+    //concatenate 3 strings and print
+    //split 50 chars string into 3 chars strings
+    //check 2 strings are equal
+
+}
