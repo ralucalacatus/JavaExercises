62647130a8176eca1042291570bc18206a6966b6
Raluca Lacatus <raluca.lacatusM@yahoo.com>
2020-05-28 22:04:41 +0300

    numbers

---
+++ src/com/exercise/datatypes/NumbersTests.java
@@ -0,0 +1,9 @@
+package com.exercise.datatypes;
+
+public class NumbersTests {
+
+    // Write a Java program that reads in two floating-point numbers and tests whether they are the same up to three decimal places.
+    // sum, divide and subtract 2 numbers
+    // sum first 50 natural numbers
+    //
+}
